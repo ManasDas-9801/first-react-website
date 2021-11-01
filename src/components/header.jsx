08eb24f202235c0fa25ffeb6139c647ee6e6d2d0
ManasDas-9801 <heyiammanas@gmail.com>
2021-11-01 21:25:01 +0530

    Header change

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,20 +5,23 @@ function header(){
    
     return(
        <React.Fragment>
-              <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                   <div className="container">
-                       <h2 className="navbar-brand">React</h2>
-                       <form className="d-flex ms-auto">
-                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                             <button className="btn btn-danger" type="submit">Search</button>
-                           </form>
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item"><p  className="nav-link">Home</p></li>
-                            <li className="nav-item"><p  className="nav-link">About</p></li>
-                        </ul>
-                       
-                       </div>
-              </nav>
+             <nav className="navbar navbar-expand-lg navbar-light bg-light">
+             <div className="container-fluid ms-5">
+    <h2 className="navbar-brand ms-5" href="#">My First react app</h2>
+     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse ms-5" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      
+      </ul>
+      <form className="d-flex me-5">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
            
        </React.Fragment>
     );
